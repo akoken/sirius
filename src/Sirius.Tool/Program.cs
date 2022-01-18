2d8151a204d0c9136ce6158b7fee25892d2a88ef
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-01-18 23:47:56 +0300

    Make Sirius project class library
    * Make compiler related code class library.
    * Take input from seperate project.
    * Expose evaluator API via Compilation class.

---
+++ src/Sirius.Tool/Program.cs
@@ -0,0 +1,86 @@
+﻿using Sirius.CodeAnalysis;
+using Sirius.CodeAnalysis.Syntax;
+
+namespace Sirius.Tool;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        bool showTree = false;
+        while (true)
+        {
+            Console.Write(">");
+
+            string line = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return;
+            }
+
+            if (line == "#showTree")
+            {
+                showTree = !showTree;
+                Console.WriteLine(showTree ? "Showing parse trees..." : "Not showing parse trees...");
+                continue;
+            }
+            else if (line == "#cls")
+            {
+                Console.Clear();
+                continue;
+            }
+
+            SyntaxTree syntaxTree = SyntaxTree.Parse(line);
+            var compilation = new Compilation(syntaxTree);
+            EvaluationResult result = compilation.Evaluate();
+            IReadOnlyList<string> diagnostics = result.Diagnostics;
+
+            if (showTree)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                PrettyPrint(syntaxTree.Root);
+                Console.ResetColor();
+            }
+
+            if (!diagnostics.Any())
+            {
+                Console.WriteLine(result.Value);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                foreach (var diagnostic in diagnostics)
+                {
+                    Console.WriteLine(diagnostic);
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+
+    private static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
+    {
+        var marker = isLast ? "└──" : "├──";
+
+        Console.Write(indent);
+        Console.Write(marker);
+        Console.Write(node.Kind);
+
+        if (node is SyntaxToken t && t.Value != null)
+        {
+            Console.Write(" ");
+            Console.Write(t.Value);
+        }
+
+        Console.WriteLine();
+
+        indent += isLast ? " " : "│ ";
+
+        SyntaxNode lastChild = node.GetChildren().LastOrDefault();
+
+        foreach (var child in node.GetChildren())
+        {
+            PrettyPrint(child, indent, child == lastChild);
+        }
+    }
+}
---
+++ src/Sirius/CodeAnalysis/Compilation.cs
@@ -0,0 +1,29 @@
+﻿using Sirius.CodeAnalysis.Binding;
+using Sirius.CodeAnalysis.Syntax;
+
+namespace Sirius.CodeAnalysis;
+
+public class Compilation
+{
+    public Compilation(SyntaxTree syntaxTree)
+    {
+        SyntaxTree = syntaxTree;
+    }
+
+    public SyntaxTree SyntaxTree { get; }
+
+    public EvaluationResult Evaluate()
+    {
+        var binder = new Binder();
+        var boundExpression = binder.BindExpression(SyntaxTree.Root);
+
+        var diagnostics = SyntaxTree.Diagnostics.Concat(binder.Diagnostics).ToArray();
+        if (diagnostics.Length > 0)
+            return new EvaluationResult(diagnostics, null);
+
+        var evaluator = new Evaluator(boundExpression);
+        var value = evaluator.Evaluate();
+
+        return new EvaluationResult(Array.Empty<string>(), value);
+    }
+}
