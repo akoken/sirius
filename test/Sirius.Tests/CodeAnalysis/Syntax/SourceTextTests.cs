23420c0df8534326f0ef8da6a8d67a1f97308b08
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-03-08 00:56:11 +0300

    Add test coverage for SourceText
    Tags:unit-test

---
+++ test/Sirius.Tests/CodeAnalysis/Syntax/SourceTextTests.cs
@@ -0,0 +1,17 @@
+﻿using Sirius.CodeAnalysis.Text;
+using Xunit;
+
+namespace Sirius.Tests.CodeAnalysis.Syntax;
+
+public class SourceTextTests
+{
+    [Theory]
+    [InlineData(".", 1)]
+    [InlineData(".\r\n", 2)]
+    [InlineData(".\r\n\r\n", 3)]
+    public void SourceText_IncludesLastLine(string text, int expectedLineCount)
+    {
+        var sourceText = SourceText.From(text);
+        Assert.Equal(expectedLineCount, sourceText.Lines.Length);
+    }
+}
