90a88e2c32dc3aebd2854cf5ab41e291ab590142
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-03-26 00:45:57 +0300

    Add ability to output the bound tree

--- src/Sirius/CodeAnalysis/Binding/BoundNode.cs
+++ src/Sirius/CodeAnalysis/Binding/BoundNode.cs
@@ -1,4 +1,6 @@
-﻿namespace Sirius.CodeAnalysis.Binding;
+﻿using System.Reflection;
+
+namespace Sirius.CodeAnalysis.Binding;
 
 internal abstract class BoundNode
 {
@@ -6,70 +8,124 @@ internal abstract class BoundNode
 
     public IEnumerable<BoundNode> GetChildren()
     {
-        var properties = GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+        var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
         foreach (var property in properties)
         {
             if (typeof(BoundNode).IsAssignableFrom(property.PropertyType))
             {
-                BoundNode child = (BoundNode)property.GetValue(this);
-                if (child is not null)
+                var child = (BoundNode)property.GetValue(this);
+                if (child != null)
                     yield return child;
             }
-            else if (typeof(BoundNode).IsAssignableFrom(property.PropertyType))
+            else if (typeof(IEnumerable<BoundNode>).IsAssignableFrom(property.PropertyType))
             {
-                IEnumerable<BoundNode> children = (IEnumerable<BoundNode>)property.GetValue(this);
+                var children = (IEnumerable<BoundNode>)property.GetValue(this);
                 foreach (var child in children)
                 {
-                    if (child is not null)
+                    if (child != null)
                         yield return child;
                 }
             }
         }
     }
 
-    public void WriteTo(TextWriter textWriter) => PrettyPrint(textWriter, this);
-
-    public override string ToString()
+    private IEnumerable<(string Name, object Value)> GetProperties()
     {
-        using var writer = new StringWriter();
-        WriteTo(writer);
-        return writer.ToString();
+        var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            if (property.Name == nameof(Kind) ||
+                property.Name == nameof(BoundBinaryExpression.Op))
+                continue;
+
+            if (typeof(BoundNode).IsAssignableFrom(property.PropertyType) ||
+                typeof(IEnumerable<BoundNode>).IsAssignableFrom(property.PropertyType))
+                continue;
+
+            var value = property.GetValue(this);
+            if (value != null)
+                yield return (property.Name, value);
+        }
     }
 
-    private static void PrettyPrint(TextWriter textWriter, BoundNode node, string indent = "", bool isLast = true)
+    public void WriteTo(TextWriter writer)
     {
-        bool isToConsole = textWriter == Console.Out;
+        PrettyPrint(writer, this);
+    }
+
+    private static void PrettyPrint(TextWriter writer, BoundNode node, string indent = "", bool isLast = true)
+    {
+        var isToConsole = writer == Console.Out;
         var marker = isLast ? "└──" : "├──";
 
         if (isToConsole)
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-        textWriter.Write(indent);
-        textWriter.Write(marker);
+        writer.Write(indent);
+        writer.Write(marker);
 
-        WriteNode(textWriter, node);
+        if (isToConsole)
+            Console.ForegroundColor = GetColor(node);
+
+        var text = GetText(node);
+        writer.Write(text);
+
+        var isFirstProperty = true;
+
+        foreach (var p in node.GetProperties())
+        {
+            if (isFirstProperty)
+                isFirstProperty = false;
+            else
+            {
+                if (isToConsole)
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                writer.Write(",");
+            }
+
+            writer.Write(" ");
+
+            if (isToConsole)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            writer.Write(p.Name);
+
+            if (isToConsole)
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            writer.Write(" = ");
+
+            if (isToConsole)
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            writer.Write(p.Value);
+        }
 
         if (isToConsole)
             Console.ResetColor();
 
-        textWriter.WriteLine();
+        writer.WriteLine();
 
-        indent += isLast ? " " : "│ ";
+        indent += isLast ? "   " : "│  ";
 
-        BoundNode lastChild = node.GetChildren().LastOrDefault();
+        var lastChild = node.GetChildren().LastOrDefault();
 
         foreach (var child in node.GetChildren())
-        {
-            PrettyPrint(textWriter, child, indent, child == lastChild);
-        }
+            PrettyPrint(writer, child, indent, child == lastChild);
     }
 
-    private static void WriteNode(TextWriter writer, BoundNode node)
+    private static string GetText(BoundNode node)
     {
-        Console.ForegroundColor = GetColor(node);
-        writer.Write(node.Kind);
-        Console.ResetColor();
+        if (node is BoundBinaryExpression b)
+            return b.Op.Kind.ToString() + "Expression";
+
+        if (node is BoundUnaryExpression u)
+            return u.Op.Kind.ToString() + "Expression";
+
+        return node.Kind.ToString();
     }
 
     private static ConsoleColor GetColor(BoundNode node)
@@ -81,5 +137,14 @@ internal abstract class BoundNode
             return ConsoleColor.Cyan;
 
         return ConsoleColor.Yellow;
+    }
+
+    public override string ToString()
+    {
+        using (var writer = new StringWriter())
+        {
+            WriteTo(writer);
+            return writer.ToString();
+        }
     }
 }
