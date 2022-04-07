ac3b6c61ac13612916463c71f556c3b98ef9caf6
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-04-08 00:10:03 +0300

    Allow REPL to force evaluation
    Sometimes our heuristic for detecting whether the submission is complete
    doesn't work, in which case we want the user to have the ability to
    force the completion. We do this by adding two blank lines.

--- src/Sirius.Tool/SiriusRepl.cs
+++ src/Sirius.Tool/SiriusRepl.cs
@@ -65,6 +65,15 @@ internal sealed class SiriusRepl : Repl
         if (string.IsNullOrEmpty(text))
             return true;
 
+        var lastTwoLinesAreBlank = text.Split(Environment.NewLine)
+            .Reverse()
+            .TakeWhile(s => string.IsNullOrEmpty(s))
+            .Take(2)
+            .Count() == 2;
+
+        if (lastTwoLinesAreBlank)
+            return true;
+
         var syntaxTree = SyntaxTree.Parse(text);
 
         if (syntaxTree.Root.Statement.GetLastToken().IsMissing)
